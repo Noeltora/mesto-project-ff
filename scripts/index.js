138868ee3989bba283f537490a3c2690075e107d
Fedor Tsukanov <f.tsukanoff@yandex.ru>
2024-05-13 22:20:00 +0300

    1st commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,39 @@
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
+const container = document.querySelector(".content");
+const cardsContainer = container.querySelector(".places__list");
+const addButton = container.querySelector(".profile__add-button");
 
 // @todo: Функция создания карточки
 
+function addCard(cardData, deleteFn) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__title").alt = cardData.name;
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+
+  deleteButton.addEventListener("click", () => {
+    deleteFn(cardElement);
+  });
+
+  return cardElement;
+}
+
 // @todo: Функция удаления карточки
 
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((card) => {
+  const result = addCard(card, deleteCard);
+  cardsContainer.append(result);
+});
